4028d5808d6c597592dcef0aeb163aedacb633ac
Tomasz Heimowski <tomek.heimowski@gmail.com>
2019-10-03 17:43:29 +0200

    Client - send request for DeleteCommand

--- src/Client/Client.fs
+++ src/Client/Client.fs
@@ -67,6 +67,7 @@ let fetchTodos () = fetch HttpMethod.GET todos None
 let request (command: Command) =
     match command with
     | AddCommand addDTO -> fetch HttpMethod.POST todos (Some addDTO)
+    | DeleteCommand id -> fetch HttpMethod.DELETE (todo id) None
 
 // Initial Model and Elmish Cmd
 
